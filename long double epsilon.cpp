3a2fe2473bf1b26d3fc92b377c36a80645037d19
Aiidanaqq <150505764+Aiidanaqq@users.noreply.github.com>
2024-09-13 15:10:19 +0600

    long double epsilon.cpp

---
+++ long double epsilon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=0;
+    long double epsilon=1.0; 
+
+    while (1.0+epsilon>1.0) {
+        n++;
+        epsilon=epsilon/10.0;
+    }
+    cout<<n<<endl;
+    return 0;
+}
